2b17a074f5b1b032d215d2e19c38e8a690d98819
hkt <9980117>
2023-08-04 23:52:40 +0800

    基于2.2.4版本建立学习分支

---
+++ study/src/main/java/com/plainhu/study/DemoConstant.java
@@ -0,0 +1,17 @@
+package com.plainhu.study;
+
+import cn.hutool.core.text.StrPool;
+
+/**
+ * @Author plainhu
+ * @Date 2023/8/4 23:04
+ * @Description: TODO
+ */
+public class DemoConstant {
+
+    public final static String SERVER_IP = "127.0.0.1";
+
+    public final static Integer SERVER_PORT = 8848;
+
+    public final static String SERVER_ADDRESS = SERVER_IP + StrPool.COLON + SERVER_PORT;
+}
---
+++ study/src/main/java/com/plainhu/study/DiscoverDemo.java
@@ -0,0 +1,30 @@
+package com.plainhu.study;
+
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingFactory;
+import com.alibaba.nacos.api.naming.NamingService;
+
+import java.util.Properties;
+
+/**
+ * @Author plainhu
+ * @Date 2023/8/4 22:54
+ * @Description: TODO
+ */
+public class DiscoverDemo {
+
+
+    public static void main(String[] args) throws NacosException, InterruptedException {
+        register();
+        Thread.sleep(60000);
+    }
+
+
+    static void register() throws NacosException {
+        Properties properties = new Properties();
+        properties.put("serverAddr", DemoConstant.SERVER_ADDRESS);
+        NamingService namingService = NamingFactory.createNamingService(properties);
+        namingService.registerInstance(DiscoverDemo.class.getTypeName(), DemoConstant.SERVER_IP, DemoConstant.SERVER_PORT);
+    }
+
+}
